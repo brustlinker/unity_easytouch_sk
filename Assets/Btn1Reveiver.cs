fcdf6da616d03161bfd2039d673b7999ddbc1cf7
dongshen <1634694819@qq.com>
2016-09-25 16:22:57 +0800

    message模式

---
+++ Assets/Btn1Reveiver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Btn1Reveiver : MonoBehaviour {
+
+	void DownMethod(string btnName)
+	{
+		print(btnName+"Down");
+	}
+
+	void PressMethod(string btnName)
+	{
+		print(btnName+"Press");
+	}
+
+	void UpMethod(string btnName)
+	{
+		print(btnName+"Up");
+	}
+}
