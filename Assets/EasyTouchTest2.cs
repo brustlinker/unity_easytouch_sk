19e643b710562cfce99c910a481593e8a716f547
dongshen <1634694819@qq.com>
2016-09-25 17:48:47 +0800

    EasyTouchAutoSelected

---
+++ Assets/EasyTouchTest2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyTouchTest2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void On_TouchStart(Gesture g)
+	{
+		Debug.Log("TouchStart"+g.position);
+	}
+}
